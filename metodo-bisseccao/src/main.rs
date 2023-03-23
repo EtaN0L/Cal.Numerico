46ee46f277a9ec70149e05c2c03444f741111e36
EtaN0L <christophe4x4@outlook.com>
2023-03-23 19:19:13 -0300

    ok

--- metodo-bisseccao/src/main.rs
+++ metodo-bisseccao/src/main.rs
@@ -1,4 +1,22 @@
+use std::io;
 fn main() {
+    let mut a = String::new();
+    let mut b = String::new();
+    let mut erro = String::new();
 
+    println!("Digite o limite a:");
+    io::stdin()
+    .read_line(&mut a)
+    .expect("Falha ao ler.");
+
+    println!("Digite o limite b:");
+    io::stdin()
+    .read_line(&mut b)
+    .expect("Falha ao ler.");
+
+    println!("Digite a tolerancia:");
+    io::stdin()
+    .read_line(&mut erro)
+    .expect("Falha ao ler.");
 }
 
