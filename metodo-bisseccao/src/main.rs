9c73ea50cd70cbdd083392237da699f075f40c10
EtaN0L <christophe4x4@outlook.com>
2023-03-24 00:06:30 -0300

    All OK

--- metodo-bisseccao/src/main.rs
+++ metodo-bisseccao/src/main.rs
@@ -1,5 +1,5 @@
-use std::io;
-fn main() {
+use std::{io, process::exit};
+fn main() {// Início da entrada de dados
     let mut a = String::new();
     let mut b = String::new();
     let mut erro = String::new();
@@ -20,26 +20,14 @@ fn main() {
     io::stdin()
     .read_line(&mut erro)
     .expect("Falha ao ler.");
-    let erro:f64 = erro.trim().parse().expect("Erro1.3");
+    let erro:f64 = erro.trim().parse().expect("Erro1.3"); // Fim da entrada de dados
 
-    let mut i=0;
-    let mut c = (a+b)/2.0;
-    if abs(b-a) < erro {
-        println!("O valor de c é: {}, foram {} interações.", c,i);
-    }else {
-        while abs(b-a) > erro {
-            let mut c = (a+b)/2.0;
-            println!("o valor de c{}",c);
-            if (funcao(a) * funcao(c)) < 0.0 {
-                b = c;
-            }else {
-                a = c;
-            }
-            
-            i+=1;
-        }
+    if funcao(a) * funcao(b) > 0.0 { // Verifica se o intervalo é aceitavel, crasha o app se não for
+        println!("Intervalo não aplicável.");
+        exit(0);
     }
-    
+
+    println!("A raiz aproximada é: {:.3}",bissec(a, b, erro));// printa a raiz
 
 }
 
@@ -47,7 +35,28 @@ fn funcao(x:f64) -> f64{
     return (x*x*x)-(9.0*x)+3.0; //Função definida como x³-9x+3
 }
 
-fn abs(x:f64) -> f64 {
+fn abs(x:f64) -> f64 {//função para calcular o valor absoluto de um número(módulo)
     let y = if x >= 0.0 { x } else { -x };
     y
 }
+
+fn bissec(mut x:f64,mut y:f64,E:f64) -> f64 { // O método da bisseção em si
+    let mut fx = funcao(x);
+    let mut fy = funcao(y);
+    let mut c:f64;
+    while abs(y-x) > E { // Enquanto |b-a| for maior que a tolerancia ele vai rodar
+        c = (x+y)/2.0;        
+        let mut fc = funcao(c);
+        if fx * fc < 0.0 {
+            y = c;
+            fy = fc;
+        }else {
+            x = c;
+            fx = fc;
+        }
+    }
+    c = (x + y)/2.0;
+    return c;    
+}
+
+
