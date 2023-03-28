f115ba25646b804c70c3826e48a903b3bf922ce5
EtaN0L <christophe4x4@outlook.com>
2023-03-28 19:10:55 -0300

    falsa posição ok

---
+++ ponto-fixo/target/debug/deps/ponto_fixo-5d10f7772ad9fa4e.d
@@ -0,0 +1,5 @@
+c:\Users\chris\Documents\Trabs IFCE\Cálculo Numérico\Cal.Numerico\ponto-fixo\target\debug\deps\ponto_fixo-5d10f7772ad9fa4e.rmeta: src\main.rs
+
+c:\Users\chris\Documents\Trabs IFCE\Cálculo Numérico\Cal.Numerico\ponto-fixo\target\debug\deps\ponto_fixo-5d10f7772ad9fa4e.d: src\main.rs
+
+src\main.rs:
